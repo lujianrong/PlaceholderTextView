a598943ed8a0cba99ddebaeb3b36ec6e9e1156d7
lujianrong <JR_ios@126.com>
2016-06-21 11:42:45 +0800

    Update TextViewTool.h

--- TextViewTool.h
+++ TextViewTool.h
@@ -2,8 +2,8 @@
 //  TextViewTool.h
 //  WellnessClub
 //
-//  Created by NewYuLong on 15/12/28.
-//  Copyright © 2015年 sky. All rights reserved.
+//  Created by lujianrong on 16/5/6.
+//  Copyright © 2016年 lujianrong. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
